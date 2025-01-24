a36f82cd5c1dc5ec96db11a37d5627e986804d31
clark-ti-mg <153373604+clark-ti-mg@users.noreply.github.com>
2025-01-24 20:55:49 -0300

    Update App.java
    1 - Modificado o enunciado para instruir melhor o usuário;
    2 - Modificada a estrutura condicional de if/else para switch/case.

--- App.java
+++ App.java
@@ -4,25 +4,26 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Bem vindo ao programa Separa Fila!");
-        System.out.println("Informe uma sequência de nome e letra correspondente a uma opção válida (A, B ou X) em seguida, separados por espaço para definir as filas.");
-        System.err.println("Exemplo: \nLuis B Pedro A Luisa A Joao X Jose X Miguel B");
+        System.out.println("\n*Para encerrar o programa, ao invés de um nome e operação, digite: S");
+        System.out.println("\nInforme uma sequência de nome e letra correspondente a uma opção válida (A, B ou X) em seguida, separados por espaço para definir as filas.");
+        System.err.println("Exemplo de sequência com uma entrada por linha: \nLuis B \nPedro A \nLuisa A \nJoao X \nJose X \nMiguel B");
 
-        //String op = "";
+        // String op = "";
 
         Fila<Usuario> q = new Fila<>();
 
         String nomeOp = "";
 
-        while(!nomeOp.equals("S") && !nomeOp.equals("s")){
-            System.out.println("Nome e operação:");
+        while (!nomeOp.equals("S") && !nomeOp.equals("s")) {
+            System.out.println("\nNome e operação:");
             nomeOp = teclado.nextLine();
 
-            if(nomeOp.split(" ").length>1){
+            if (nomeOp.split(" ").length > 1) {
                 String nome = nomeOp.split(" ")[0];
                 String op = nomeOp.split(" ")[1];
                 q.enfileirar(new Usuario(nome, op));
             }
-            
+
         }
 
         Fila<Usuario> filaA = new Fila<>();
@@ -30,29 +31,36 @@ public class App {
 
         int tamanhoQ = q.getTamanho();
 
-        for(int i = 0; i < tamanhoQ; i++){
+        System.out.println("\nOrdem de entrada nas filas:");
+
+        for (int i = 0; i < tamanhoQ; i++) {
             Usuario u = q.desenfileirar();
-            if(u.operacao.equals("A")){
-                filaA.enfileirar(u);
-                System.out.println(u.nome + " entrou na fila A.");
-            }
-            if(u.operacao.equals("B")){
-                filaB.enfileirar(u);
-                System.out.println(u.nome + " entrou na fila B.");
-            }
-            if(u.operacao.equals("X")){
-                if(filaA.getTamanho() == filaB.getTamanho()){
-                    System.out.println(u.nome + " descartado.");
-                }else if(filaA.getTamanho() > filaB.getTamanho()) {
-                    filaB.enfileirar(u);
-                    System.out.println(u.nome + " entrou na fila B.");
-                }else if(filaA.getTamanho() < filaB.getTamanho()){
+            switch (u.operacao) {
+                case "A":
                     filaA.enfileirar(u);
                     System.out.println(u.nome + " entrou na fila A.");
-                }
+                    break;
+
+                case "B":
+                    filaB.enfileirar(u);
+                    System.out.println(u.nome + " entrou na fila B.");
+                    break;
+
+                case "X":
+                    if (filaA.getTamanho() == filaB.getTamanho()) {
+                        System.out.println(u.nome + " descartado.");
+                    } else if (filaA.getTamanho() > filaB.getTamanho()) {
+                        filaB.enfileirar(u);
+                        System.out.println(u.nome + " entrou na fila B.");
+                    } else if (filaA.getTamanho() < filaB.getTamanho()) {
+                        filaA.enfileirar(u);
+                        System.out.println(u.nome + " entrou na fila A.");
+                    }
+                    break;
+                default:
+                    System.out.println("Operação inválida para " + u.nome + ".");
+                    break;
             }
         }
-
-        
     }
 }
